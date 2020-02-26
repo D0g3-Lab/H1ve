adef2e91a04c3630d82e77ddc497f0e903260e0b
0akarma <ak_harold@outlook.com>
2020-02-26 22:23:57 +0800

    Fix some bugs for auto deploy user guide

---
+++ docs/docs/.vuepress/config.js
@@ -0,0 +1,58 @@
+module.exports = {
+    title: 'H1ve',
+    description: 'D0g3 @ 道格安全研究实验室',
+    themeConfig: {
+      // logo: '/assets/img/logo.png',
+      nav: [
+          { text: '指南', link: '/guide/' },
+          {
+              text: '插件',
+              items: [
+                { text: 'CTFd-Owl', link: '/guide/ctfd-owl'},
+                { text: 'CTFd-Glowworm', link: '/guide/ctfd-glowworm'}
+              ]
+            }
+        ],
+      sidebar: [
+          {
+              title: '指南',
+            //   path: '/guide/',
+              collapsable: false, // 可选的, 默认值是 true,
+              sidebarDepth: 2,    // 可选的, 默认值是 1
+              children: [
+                '/guide/run',
+                '/guide/troubles'
+              ]
+          },
+          {
+              title: '插件',
+              collapsable: false, // 可选的, 默认值是 true,
+              sidebarDepth: 2,    // 可选的, 默认值是 1
+              children: [
+                '/guide/ctfd-owl',
+                '/guide/ctfd-glowworm'
+              ]
+          }
+      ],
+      lastUpdated: 'Last Updated',
+      displayAllHeaders: true,
+      sidebarDepth: 2,
+      smoothScroll: true,
+      // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+      repo: 'https://github.com/D0g3-Lab/H1ve',
+      // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+      // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+      repoLabel: 'GitHub',
+      // docsDir: 'docs',
+      // 假如文档放在一个特定的分支下：
+      docsBranch: 'dev',
+      // 默认是 false, 设置为 true 来启用
+      editLinks: true,
+      // 默认为 "Edit this page"
+      editLinkText: '在 GitHub 上编辑此页'
+    },
+    markdown: {
+        lineNumbers: true
+    }
+  }
+  
---
+++ docs/docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: /hive-logo.jpg
+heroText: H1ve
+tagline: An Easy / Quick / Cheap Integrated AD Platform
+actionText: 快速上手 →
+actionLink: /guide/
+features:
+- title: Easy
+  details: 简易
+- title: Quick
+  details: 快速
+- title: Cheap
+  details: 低成本
+footer: Apache License 2.0 | Copyright © 2019-2020 D0g3
+---
